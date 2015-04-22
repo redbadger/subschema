35fee690bf1b186d1f50fe5b9318268506fd2106
juspears <juspears@paypal.com>
2015-04-22 09:42:59 -0700

    Updated hash 87e803107128e301276cd87bf6d6226a

---
+++ hash.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+FILE=${1:-../subschema/.build/app}
+HASH=$(md5 -q ${FILE}.js)
+BASE=$(basename $FILE)
+cp ${FILE}.js ${BASE}.${HASH}.js && \
+cp ${FILE}.js.map ${BASE}.${HASH}.js.map && \
+sed "s,\(src=\"\).*\(\.js\"\),\1${BASE}\.${HASH}\2,"<index.html.tmpl>index.html && \
+git add ${BASE}.${HASH}.js* && \
+git commit -a -m "Updated hash ${HASH}" && \
+echo "push $HASH now"
