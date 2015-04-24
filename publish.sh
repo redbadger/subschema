7ae22248f1f5f0e692c82b2e7b8b6c3b2b842545
juspears <juspears@paypal.com>
2015-04-24 11:05:41 -0700

    Updated hash ca91f1dccf243a7989f31263ef9302a8

--- publish.sh
+++ publish.sh
@@ -4,6 +4,19 @@ FILE=${1:-../subschema/.build/app}
 HASH=$(md5 -q ${FILE}.js)
 BASE=$(basename $FILE)
 OPWD=$PWD
+function check(){
+	open index.html &
+}
+
+
+function push(){
+	git add ${BASE}.${HASH}.{js,js.map} && \	
+	git commit -a -m "Updated hash ${HASH}" && \
+	git status && \	
+	git push origin gh-pages && \
+	echo "gh-pages now running $HASH " && \
+	echo "http://jspears.github.io/subschema"
+}
 
 rm -f ./${BASE}.*.{js,js.map}
 cd $DIR && \
@@ -12,4 +25,9 @@ cd $OPWD && \
 cp ${FILE}.js.map ${BASE}.${HASH}.js.map && \
 sed "s,\(src=\"\).*\(\.js\"\),\1${BASE}\.${HASH}\2,"<$DIR/public/index.html>index.html && \
 sed "s,\(\/\/# sourceMappingURL=\).*\(.map.*\),\1${BASE}.${HASH}.js\2,"<${FILE}.js>${BASE}.${HASH}.js 
-echo "Now run push.sh"
+check
+read -p "Do you want to push[y|Y]" dp;
+case $dp in
+	y|Y) push; break;;
+	n|N) echo "Don't forget to publish"; exit 0;;
+esac
