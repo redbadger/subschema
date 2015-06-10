52a5ed9adc235c5fcbcf0cd5c8592a0fbec8d495
juspears <juspears@paypal.com>
2015-06-10 10:00:41 -0700

    Updated hash 53fe3f8547321e4e4c57

--- publish.sh
+++ publish.sh
@@ -1,30 +1,29 @@
 #!/usr/bin/env bash
 DIR=${1:-../subschema}
-FILE=${1:-../subschema/.build/app}
-HASH=$(md5 -q ${FILE}.js)
-BASE=$(basename $FILE)
 OPWD=$PWD
+HASH=
 function check(){
 	open index.html &
 }
 
 
 function push(){
-	git add ${BASE}.${HASH}.{js,js.map} && \	
+	git add app.${HASH}.{js,js.map} && \
 	git commit -a -m "Updated hash ${HASH}" && \
 	git status && \	
 	git push origin gh-pages && \
 	echo "gh-pages now running $HASH " && \
 	echo "http://jspears.github.io/subschema"
 }
-
+function build(){
+	cd $DIR
+	npm run build-demo
+	cd $OPWD
+	HASH=$(ls app.*.js | sed 's,app\.\(.*\)\.js,\1,g' )
+}
 rm -f ./${BASE}.*.{js,js.map}
-cd $DIR && \
-NODE_ENV=production npm run webpack && \
-cd $OPWD && \
-cp ${FILE}.js.map ${BASE}.${HASH}.js.map && \
-sed "s,\(src=\"\).*\(\.js\"\),\1${BASE}\.${HASH}\2,"<$DIR/public/index.html>index.html && \
-sed "s,\(\/\/# sourceMappingURL=\).*\(.map.*\),\1${BASE}.${HASH}.js\2,"<${FILE}.js>${BASE}.${HASH}.js 
+build  && \
+sed "s,\(src=\"\).*\(\.js\"\),\1app\.${HASH}\2,"<$DIR/public/index.html>index.html
 check
 read -p "Do you want to push[y|Y]" dp;
 case $dp in
